265835517f856eb5a93debbbc8357613204c4d6d
Rakshithgt <114352972+Rakshithgt@users.noreply.github.com>
2025-02-22 13:48:33 +0530

    Update infra.tf

--- development/infra.tf
+++ development/infra.tf
@@ -5,7 +5,7 @@ module "dev_vpc_1" {
   environment        = "Development"
   public_cidr_block  = ["10.90.1.0/24", "10.90.2.0/24", "10.90.3.0/24"]
   private_cidr_block = ["10.90.10.0/24", "10.90.20.0/24", "10.90.30.0/24"]
-  azs                = ["ap-south-1a", "ap-south-1b", "ap-south-1c"]
+  azs                = ["ap-south-1a", "ap-south-1b"]
   natgw_id           = module.dev_natgw_1.natgw_id
 }
 
